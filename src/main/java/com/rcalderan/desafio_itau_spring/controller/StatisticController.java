b63b8b95fb51a7715f57c2970a634a96239cd988
Richard Calderan <richardcck@hotmail.com>
2025-02-07 14:05:17 -0300

    Swagger basic implementation with some log

--- src/main/java/com/rcalderan/desafio_itau_spring/controller/StatisticController.java
+++ src/main/java/com/rcalderan/desafio_itau_spring/controller/StatisticController.java
@@ -2,6 +2,11 @@ package com.rcalderan.desafio_itau_spring.controller;
 
 import com.rcalderan.desafio_itau_spring.dto.StatisticDTO;
 import com.rcalderan.desafio_itau_spring.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/estatistica")
 public class StatisticController {
 
+    private static final Logger logger = LogManager.getLogger(StatisticController.class);
+
     @Autowired
     private TransactionService service;
 
+    @Operation(summary = "Get transaction statistics in last minute")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Last minute statistics"),
+            @ApiResponse(responseCode = "500", description = "Internal error")
+    })
     @GetMapping
     public ResponseEntity<StatisticDTO> getLastMinStatistic(){
-        int TIME = 60;
-        return new ResponseEntity<>(service.statistic(TIME), HttpStatus.OK);
+        try{
+            int TIME = 60;
+            return new ResponseEntity<>(service.statistic(TIME), HttpStatus.OK);
+        } catch (Exception e) {
+
+            logger.error("Create trasaction error: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/rcalderan/desafio_itau_spring/controller/TransactionController.java
+++ src/main/java/com/rcalderan/desafio_itau_spring/controller/TransactionController.java
@@ -3,7 +3,12 @@ package com.rcalderan.desafio_itau_spring.controller;
 import com.rcalderan.desafio_itau_spring.model.Transaction;
 import com.rcalderan.desafio_itau_spring.repository.TransactionRepository;
 import com.rcalderan.desafio_itau_spring.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/transacao")
 public class TransactionController {
+    private static final Logger logger = LogManager.getLogger(TransactionController.class);
 
     @Autowired
     private TransactionService transactionService;
@@ -26,23 +32,50 @@ public class TransactionController {
      *     "dataHora": "2020-08-07T12:34:56.789-03:00"
      * }
      */
+
+    @Operation(summary = "Create a new transaction")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transaction accepted"),
+            @ApiResponse(responseCode = "422", description = "Not accepted"),
+            @ApiResponse(responseCode = "500", description = "Internal error")
+    })
     @PostMapping
     public ResponseEntity<Void> createTransaction(@RequestBody @Valid Transaction transaction){
-        if (transaction.hasErrors()) {
-            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
-        }else{
-            return new ResponseEntity<>(HttpStatus.CREATED);
+        try{
+            if (transaction.hasErrors()) {
+                logger.info("Transaction rejected: {}",transaction);
+                return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+            }else{
+                logger.info("Transaction accepted: {}",transaction);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            }
+        } catch (Exception e) {
+            logger.error("Create trasaction error: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     }
 
+    @Operation(summary = "Delete all transactions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All transaction clear"),
+            @ApiResponse(responseCode = "204", description = "Nothing to clear"),
+            @ApiResponse(responseCode = "500", description = "Internal error")
+    })
     @DeleteMapping
     public ResponseEntity<List<Transaction>> deleteTransaction(){
-        List<Transaction> deleted = transactionService.deleteAll();
-        if(deleted!= null && !deleted.isEmpty()){
-            return new ResponseEntity<>(deleted, HttpStatus.OK);
+        try{
+            List<Transaction> deleted = transactionService.deleteAll();
+            if(deleted!= null && !deleted.isEmpty()){
+                logger.info("All transaction deleted");
+                return new ResponseEntity<>(deleted, HttpStatus.OK);
+            }
+            logger.info("Trying to delete but nothing to clear");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e) {
+            logger.error("Delete transactions error: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
